415c34af2e7bd0818748aa1e2b7a9e556809de86
瓶子的跋涉 <58133210+foryouos@users.noreply.github.com>
2022-05-23 13:47:10 +0800

    新乡工程学院官方设计
    企业网站设计新乡工程学院

---
+++ homeApp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Lunbo_main,Lunbo_video
+# Register your models here.
+admin.site.site_header = '新乡工程学院后台管理系统'
+admin.site.site_title = '新乡工程学院后台管理系统'
+
+#将模拟
+class Lunbo_mainAdmin(admin.ModelAdmin):
+    list_display = ['description','photo']
+admin.site.register(Lunbo_main,Lunbo_mainAdmin)
+
+class Lunbo_videoAdmin(admin.ModelAdmin):
+    list_display = ['description','video']
+admin.site.register(Lunbo_video,Lunbo_videoAdmin)
